a57976fd45a6b973d1a2d2e22a29363286aa7df7
Joshua Escamilla <joshua.escamilla@selu.edu>
2013-04-30 02:26:19 -0500

    Rewrote using getopt, cleaned up code, fixed .gitignore

--- hexdump.c
+++ hexdump.c
@@ -3,116 +3,155 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-#define BYTES_PER_LINE 16
-#define streq(X,Y) (!strcmp(X,Y))
-
-enum mode
+enum format
 {
-	MODE_HEX_LOWER,
-	MODE_HEX_UPPER,
-	MODE_OCT,
-	MODE_DEC
+	FORMAT_DECIMAL,
+	FORMAT_HEXADECIMAL_LOWER,
+	FORMAT_HEXADECIMAL_UPPER,
+	FORMAT_OCTAL
 };
 
+void print_usage();
 void print_help();
-void hexdump(FILE *fp, int mode);
+void dump_file(FILE *fp, int format, int bytes_per_line);
 
 int main(int argc, char *argv[])
 {
 	if (argc == 1) {
-		puts("No file given");
+		print_usage();
 		return EXIT_SUCCESS;
 	}
 
-	int mode = MODE_HEX_LOWER;
-
-	for (int i = 0; i < argc; ++i) {
-		if (streq(argv[i], "-h")) {
-			mode = MODE_HEX_LOWER;
-		} else if (streq(argv[i], "-H")) {
-			mode = MODE_HEX_UPPER;
-		} else if (streq(argv[i], "-o") || streq(argv[i], "-O")) {
-			mode = MODE_OCT;
-		} else if (streq(argv[i], "-d") || streq(argv[i], "-D")) {
-			mode = MODE_DEC;
-		} else if (streq(argv[i], "--help")) {
+	for (int i = 1; i < argc; ++i) {
+		if (!strcmp("--help", argv[i])) {
 			print_help();
 			return EXIT_SUCCESS;
 		}
 	}
 
-	char *file = argv[1];
+	int format = FORMAT_HEXADECIMAL_LOWER;
+	int bytes_per_line = 8;
 
-	FILE *fp = fopen(file, "rb");
-
-	if (fp == NULL) {
-		fprintf(stderr, "Error opening file \"%s\": %s\n",
-			file, strerror(errno));
-		return EXIT_FAILURE;
+	int c;
+	while ((c = getopt(argc, argv, "b:dhHo")) != -1) {
+		switch (c) {
+			case 'b':
+				bytes_per_line = atoi(optarg);
+				if (!bytes_per_line) {
+					fprintf(stderr, "%s: invalid argument "
+						"for option -b: %s\n", argv[0],
+						optarg);
+					return EXIT_FAILURE;
+				}
+				break;
+			case 'd':
+				format = FORMAT_DECIMAL;
+				break;
+			case 'h':
+				format = FORMAT_HEXADECIMAL_LOWER;
+				break;
+			case 'H':
+				format = FORMAT_HEXADECIMAL_UPPER;
+				break;
+			case 'o':
+				format = FORMAT_OCTAL;
+				break;
+			case '?':
+				return EXIT_FAILURE;
+			default:
+				abort();
+		}
 	}
 
-	hexdump(fp, mode);
+	for (int i = optind; i < argc; ++i) {
+		const char *filename = argv[i];
+		FILE *fp = fopen(filename, "rb");
 
-	fclose(fp);
+		if (fp == NULL) {
+			fprintf(stderr, "Error opening file \"%s\": %s\n",
+				filename, strerror(errno));
+			return EXIT_FAILURE;
+		}
+
+		dump_file(fp, format, bytes_per_line);
+		fclose(fp);
+	}
 
 	return EXIT_SUCCESS;
 }
 
-void print_help()
+void print_usage()
 {
-	puts("hexdump file [-h | -H | -o | -d] [--help]");
-	puts("    -h      Display bytes in lowercase hexadecimal (default)");
-	puts("    -H      Display bytes in uppercase hexadecimal");
-	puts("    -o      Display bytes in octal");
-	puts("    -d      Display bytes in decimal");
-	puts("    --help  Show this help");
+	puts("Usage: hexdump [-dhHo] [-b count] [--help]");
 }
 
-void hexdump(FILE *fp, int mode)
+void print_help()
+{
+	print_usage();
+	putchar('\n');
+	puts("Options:");
+	puts("    -b count");
+	puts("          Display `count' bytes per line (default value: 8)");
+	puts("    -d");
+	puts("          Display data in decimal format");
+	puts("    -h");
+	puts("          Display data in lowercase hexadecimal format.");
+	puts("          This is the default behavior.");
+	puts("    -H");
+	puts("          Display data in uppercase hexadecimal format");
+	puts("    -o");
+	puts("          Display data in octal format");
+	puts("    --help");
+	puts("          Display this help and exit.");
+}
+
+void dump_file(FILE *fp, int format, int bytes_per_line)
 {
 	if (fp == NULL) return;
 
-	unsigned char bytes[BYTES_PER_LINE];
-	unsigned int bytes_read;
+	unsigned char bytes[bytes_per_line];
+	unsigned int number_bytes_read;
 	unsigned int pos;
 
 	for ( ; ; ) {
 		pos = ftell(fp);
 
-		bytes_read = fread(bytes, 1, BYTES_PER_LINE, fp);
-		if (!bytes_read) break;
+		number_bytes_read = fread(bytes, sizeof(unsigned char),
+			bytes_per_line, fp);
+		if (!number_bytes_read) break;
 
-		switch (mode) {
-			case MODE_HEX_LOWER:
+		switch (format) {
+			case FORMAT_DECIMAL:
+				printf("%08d", pos);
+				break;
+			case FORMAT_HEXADECIMAL_LOWER:
 				printf("%08x", pos);
 				break;
-			case MODE_HEX_UPPER:
+			case FORMAT_HEXADECIMAL_UPPER:
 				printf("%08X", pos);
 				break;
-			case MODE_OCT:
+			case FORMAT_OCTAL:
 				printf("%08o", pos);
-				break;
-			case MODE_DEC:
-				printf("%08d", pos);
 		}
 
 		fputs("  ", stdout);
 
-		for (int i = 0; i < BYTES_PER_LINE; ++i) {
-			if (i < bytes_read) {
-				switch (mode) {
-					case MODE_HEX_LOWER:
+		for (int i = 0; i < bytes_per_line; ++i) {
+			if (i < number_bytes_read) {
+				switch (format) {
+					case FORMAT_DECIMAL:
+						printf("%03d", bytes[i]);
+						break;
+					case FORMAT_HEXADECIMAL_LOWER:
 						printf("%02x", bytes[i]);
 						break;
-					case MODE_HEX_UPPER:
+					case FORMAT_HEXADECIMAL_UPPER:
 						printf("%02X", bytes[i]);
 						break;
-					case MODE_OCT:
+					case FORMAT_OCTAL:
 						printf("%03o", bytes[i]);
-						break;
-					case MODE_DEC:
-						printf("%03d", bytes[i]);
 				}
 				putchar(' ');
 			} else {
@@ -122,7 +161,7 @@ void hexdump(FILE *fp, int mode)
 
 		putchar(' ');
 
-		for (int i = 0; i < bytes_read; ++i) {
+		for (int i = 0; i < number_bytes_read; ++i) {
 			if (isprint(bytes[i])) {
 				putchar(bytes[i]);
 			} else {
